51ffba622fa451f329c9304a7b6b2d6a3352a1e3
Hallo <Hallo@192.168.1.139>
2019-10-20 20:14:34 +0200

    "Ejercicio 4 do Boletin 4 completo"

---
+++ boletin4_4/src/boletin4_4/Boletin4_4.java
@@ -0,0 +1,22 @@
+package boletin4_4;
+
+// @author Hallo
+public class Boletin4_4 {
+
+    public static void main(String[] args) {
+        Conta conta1 = new Conta("Pedro", "0001", 1.5, 1000);
+        conta1.amosar();
+        conta1.ingreso(250);
+        System.out.println("Saldo despois do ingreso: " + conta1.getSaldo());
+        conta1.reintegro(100);
+        System.out.println("Saldo despois do reintegro: " + conta1.getSaldo());
+
+        Conta conta2 = new Conta("Juan", "0002", 1.5, 500);
+        System.out.println("Saldo da conta1: " + conta1.getSaldo() + "\n Saldo da conta2: " + conta2.getSaldo());
+        conta1.transferencia(conta2, 200);
+        System.out.println("Saldo da conta1 despois da transferencia " + conta1.getSaldo()
+                + "\nSaldo da conta2 despois da transferencia: " + conta2.getSaldo());
+
+    }
+
+}
